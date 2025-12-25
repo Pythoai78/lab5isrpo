2f432b58b9a650c1ad0e72952f9160aab2514cff
Aidar Tashkulov <awesome.tashkulov@yandex.ru>
2025-12-25 17:22:49 +0300

    tests

---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+import unittest
+from main import add, divide
+
+class TestMainFunctions(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
